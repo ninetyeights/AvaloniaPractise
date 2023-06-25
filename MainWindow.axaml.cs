3a6c1e364bbef8706906f50c9ace262339c63a9f
Rain <clover1998love@gmail.com>
2023-06-24 22:20:05 -0400

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaPractise
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
